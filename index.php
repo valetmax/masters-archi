56c8180c3ef6e1b9053e0a200b68cff1395194ea
Maxime Valette <valettemaxime26@gmail.com>
2021-01-21 14:44:50 +0100

    init

--- index.php
+++ index.php
@@ -3,30 +3,91 @@
 const EMAIL_PARAM_NAME = 'email';
 const SPAM_DOMAINS = ['spamming.com', 'mailinator.com', 'oneminutemail.com'];
 
-if (empty($_GET) || empty($_GET[EMAIL_PARAM_NAME])) {
-  echo "Please provide a valid email address";
-  exit;
+/**
+ * Undocumented function
+ *
+ * @param string $name
+ * @return bool|string
+ */
+function getRequestParam(string $name) {
+  if (empty($_GET) || empty($_GET[$name])) {
+    return false;
+  }
+  
+  return $_GET[$name];
 }
 
-$email = $_GET['email'];
-
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  echo "Invalid email address";
-  exit;
+/**
+ * Undocumented function
+ *
+ * @param string $email
+ * @return boolean
+ */
+function isEmailValid(string $email): bool {
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    return false
+  }
+  return true;
 }
 
-$emailParts = explode('@', $email);
-
-if ($emailParts === false || count($emailParts) !== 2) {
-  echo "Unable to extract domain from email address";
-  exit;
+/**
+ * Undocumented function
+ *
+ * @param string $email
+ * @return bool|string
+ */
+function extractDomainFromEmail(string $email) {
+  $emailParts = explode('@', $email);
+  if ($emailParts === false || count($emailParts) !== 2) {
+    return false;
+  }
+  return $emailParts[1];
 }
 
-$domain = $emailParts[1];
-
-if (in_array($domain, SPAM_DOMAINS)) {
-  echo "Email is spam";
-  exit;
+/**
+ * Undocumented function
+ *
+ * @param string $domain
+ * @param array $spamDomain
+ * @return boolean
+ */
+function isSpam(string $domain, array $spamDomain): bool {
+  if (in_array($domain, SPAM_DOMAINS)) {
+    return true;
+  }
+  return false;
 }
 
-echo "Email is valid";
+
+function validateEmail() {
+  $errorsMessages = [
+    "SPAM_ERROR" => "Email is spam",
+    "INVALID_EMAIL" => "Invalid email address",
+    "EXTRACT_DOMAIN_ERROR" => "Unable to extract domain from email address",
+    "REQUEST_PARAM_ERROR" => "Please provide a valid request params"
+  ];
+  
+  $email = getRequestParam(EMAIL_PARAM_NAME);
+  $error = null;
+  if ($email) {
+    if (isEmailValid($email)) {
+      $domain = extractDomainFromEmail($email);
+      if ($domain) {
+        if (!isSpam($domain, SPAM_DOMAINS)) {
+          echo "Email is valid";
+        } else {
+          $error = $errorsMessages['SPAM_ERROR'];
+        }
+      } else {
+        $error = $errorsMessages['EXTRACT_DOMAIN_ERROR'];
+      }
+    } else {
+      $error = $errorsMessages['INVALID_EMAIL'];
+    }
+  } else {
+    $error = $errorsMessages['REQUEST_PARAM_ERROR'];
+  }
+  echo $error;
+}
+
+validateEmail();  
